995d1efa0f4f25a095c8fedc0f5378af832965bb
humaira2020 <80723718+humaira2020@users.noreply.github.com>
2021-03-21 21:52:20 +0530

    Update 1_Hello.c
    Hello World Program modified with comments

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-	int main(){
-	printf("Hello World");
-	return 0;
-	}
+//Humaira Gani       Dated:17-03-2021
+//Purpose of Program is to print Hello World
+#include<stdio.h> //Pre-processor directive to include standard input output function header file
+int main(){ //program starts from main function having integer return data type
+printf("Hello World"): //printf calling function it tells compiler to display Hello World on console 
+return 0; // output of function 
+} //Here the program stops
