094a90265c4b536bf2fc89b5103a33b25435a24a
humaira124 <2020a1r124@mietjammu.in>
2021-03-29 18:19:58 +0530

    Do While Program File

---
+++ Q_17_D0While.c
@@ -0,0 +1,12 @@
+//Humaira                               Dated:29_03_2021
+//DO While program 
+#include<stdio.h> // header function
+int main(){ //main function
+	int i=1; //intialize and Declare of i
+	do{ 
+	printf("Coding\n"); //printing output
+	i+=4; //increment by 4
+	}
+	while(i<9); //while checking condition 
+	return 0; //return statement
+}
