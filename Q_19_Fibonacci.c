6333c9f86e3840c7f7144f45c18b48d690cf7578
humaira124 <2020a1r124@mietjammu.in>
2021-03-29 22:08:00 +0530

    Fibonacci series File

---
+++ Q_19_Fibonacci.c
@@ -0,0 +1,15 @@
+//Humaira 
+#include<stdio.h>
+int main (){
+	int n, a=0, b=1, c; //variable declaration and intialization, if n=7 
+	printf("Enter the limit:");
+	scanf("%d",&n);
+	for(int i=1; i<=7; i++)
+	{
+		printf("%d\n",a); //here a=0, b=1, c=? 
+		c = a+b;
+		a = b; //value of storing in a
+		b = c; //value of c assigning to b
+	}
+		return 0;
+}s
