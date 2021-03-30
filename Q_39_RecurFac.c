a7e1149a7535c852fd13b8e01ef3d57ec43efc61
humaira124 <2020a1r124@mietjammu.in>
2021-03-30 22:04:32 +0530

    Factorial using Recursive Function

---
+++ Q_39_RecurFac.c
@@ -0,0 +1,14 @@
+//Humaira
+//Program: to calculate  Factorial using Recursive Functions 
+#include<stdio.h> // header function file 
+int main() //main function    
+{    
+ int i,fact=1,number; //Declaration of variable and initializing 'fact' variable
+ printf("Enter a number: "); //print function
+  scanf("%d",&number); //user inputs number   
+    for(i=1;i<=number;i++){   //using for loop  
+      fact=fact*i; //fact equals to fact itself multiply by 'i' variable
+  }    
+  printf("Factorial of %d is: %d",number,fact); //printing factorial     
+return 0;  
+} //end
