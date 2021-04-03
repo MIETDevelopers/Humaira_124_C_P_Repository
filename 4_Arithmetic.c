df055bfc2b82dbcd2029e6a3b5d573bd37f0c666
humaira2020 <80723718+humaira2020@users.noreply.github.com>
2021-04-03 15:35:21 +0530

    Update and rename 2_Addition.c to 4_Arithmetic.c

---
+++ 4_Arithmetic.c
@@ -0,0 +1,16 @@
+//Humaira
+//Arithmetic operations program
+#include<stdio.h> //main header function file
+int main(){ //main body 
+	int h,i,add; // Declaration of variables
+	scanf("%d %d",&h,&i); //user input
+	printf("\nEnter two numbers");
+	printf("Addition: %d\n",add);
+	add = h + i; //addition of 2 numbers
+	int j,k,sub; //Declaration of variables
+	scanf("%d %d",&j,&k); //user input
+	printf("\nEnter two numbers");
+	printf("Subtraction: %d\n",sub);
+	add = j - k; //subtraction of 2 numbers
+	return 0;	
+}
