549e87b0bf338bc5acb57cc96c2d562703fdcedc
humaira124 <2020a1r124@mietjammu.in>
2021-04-04 00:55:29 +0530

    Sorting of an Array

---
+++ 17_SortArr.c
@@ -0,0 +1,40 @@
+//Humaira Gani
+//Program:Sorting an array using insertion sort
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a[] = {16, 19, 11, 15, 10, 12, 14};
+    int i,j;
+
+    for(i=0;i<7;i++)
+    {
+        j = i;
+
+        //i is not the first element
+        while(j>0)
+        {
+            //not in order
+            if(a[j-1] > a[j])
+            {
+                //swapping
+                int temp = a[j-1];
+                a[j-1] = a[j];
+                a[j] = temp;
+            }
+            //in order
+            else
+            {
+                break;
+            }
+            j--;
+        }
+    }
+
+    for(i=0;i<7;i++)
+        printf("%d\n",a[i]);
+
+    return 0;
+}
