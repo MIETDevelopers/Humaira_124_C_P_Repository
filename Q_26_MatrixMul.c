820f338d395323424ad9cf70f3b352e23966cbf1
humaira2020 <80723718+humaira2020@users.noreply.github.com>
2021-04-12 20:40:42 +0530

    Update Q_26_MatrixMul.c

--- Q_26_MatrixMul.c
+++ Q_26_MatrixMul.c
@@ -2,14 +2,6 @@
 // Purpose : Multiplication Of Matrix 2D
 #include<stdio.h> //header function file     
 int main(){ //main function  
-	int a[10][10],b[10][10],mul[10][10],r,c,i,j; //Varible declration and declaring 3 arrays of same size 100       
-	printf("enter the number of row"); //printf calling function   
-	scanf("%d",&r); //user input    
-	printf("enter the number of column"); 
-	scanf("%d",&c); //user input
-	printf("%d\t",mul[i][j]);
-	scanf("%d %d",&i,&j); //user input
-	printf("\n"); //print for new line character       
-return 0;  
+	  
 } //end 
   
